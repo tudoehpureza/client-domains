4b867f401f00f117153db1bc112632b8708dfc83
Felipe Alves de Jesus <felipe.jesus@ext.vallourec.com>
2023-06-01 19:15:11 -0300

    refactoring table

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,29 +1,33 @@
 import { defineConfig } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
 import tailwind from '@astrojs/tailwind';
-import react from "@astrojs/react";
-import netlify from "@astrojs/netlify/functions";
+import react from '@astrojs/react';
+import netlify from '@astrojs/netlify/functions';
 const DEV_PORT = 2121;
-
 
 // https://astro.build/config
 
 // https://astro.build/config
 export default defineConfig({
-  site: process.env.CI ? 'https://themesberg.github.io' : `http://localhost:${DEV_PORT}`,
-  base: process.env.CI ? '/flowbite-astro-admin-dashboard' : undefined,
-  // output: 'server',
+	site: process.env.CI
+		? 'https://themesberg.github.io'
+		: `http://localhost:${DEV_PORT}`,
+	base: process.env.CI ? '/flowbite-astro-admin-dashboard' : undefined,
+	// output: 'server',
 
-  /* Like Vercel, Netlify,… Mimicking for dev. server */
-  // trailingSlash: 'always',
+	/* Like Vercel, Netlify,… Mimicking for dev. server */
+	// trailingSlash: 'always',
 
-  server: {
-    /* Dev. server only */
-    port: DEV_PORT
-  },
-  integrations: [
-  //
-  sitemap(), tailwind(), react()],
-  output: "server",
-  adapter: netlify()
+	server: {
+		/* Dev. server only */
+		port: DEV_PORT,
+	},
+	integrations: [
+		//
+		sitemap(),
+		tailwind(),
+		react(),
+	],
+	output: 'server',
+	adapter: netlify(),
 });
