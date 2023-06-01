fa4d7a7d40ab74f14d111210408d5bf58247dfd3
Felipe Alves de Jesus <felipe.jesus@ext.vallourec.com>
2023-06-01 18:28:32 -0300

    refactoring table

--- src/app/constants.ts
+++ src/app/constants.ts
@@ -1,6 +1,6 @@
 export const API_URL = `${import.meta.env.SITE}${import.meta.env.BASE_URL}api/`;
 
-export const REMOTE_ASSETS_BASE_URL = `https://flowbite-admin-dashboard.vercel.app`;
+export const REMOTE_ASSETS_BASE_URL = `https://Tudoehpureza-admin-dashboard.vercel.app`;
 
 export const SITE_TITLE = 'Tudoehpureza';
 
